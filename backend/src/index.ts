c44a32970462d8b90311abf56b9d8eb0a666b3c6
SoerenToennesen <sorentoennesen@gmail.com>
2023-05-08 21:05:08 +0200

    Added back main index.ts to git after removing confidential info from history

---
+++ backend/src/index.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+const PropertiesReader = require('properties-reader');
+
+import router from './router';
+
+const prop = PropertiesReader('../app.properties');
+const getProperty = (pty: string) => {
+    return prop.get(pty);
+}
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+const port = parseInt(getProperty('server.port'));
+server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}/`);
+});
+
+const dbMongoUsername = getProperty('db.mongo.username');
+const dbMongoPassword = getProperty('db.mongo.password');
+const MONGO_URL = `mongodb+srv://${dbMongoUsername}:${dbMongoPassword}@cluster0.ikk0ats.mongodb.net/?retryWrites=true&w=majority`; // DB URI
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router());
