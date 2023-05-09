d73dc932f9a336bb7cf2128b81caa7de9c7676d5
SoerenToennesen <sorentoennesen@gmail.com>
2023-05-09 07:44:57 +0200

    Added account info page

---
+++ frontend/src/components/SectionBody.tsx
@@ -0,0 +1,80 @@
+import React, {} from "react";
+
+export const ReportRowCustom: React.FC<{
+    title: JSX.Element,
+    value?: JSX.Element,
+    marginFromTop?: string,
+}> = ({title, value, marginFromTop}) => {
+    return (
+        <div style={{display: "flex", justifyContent: "space-between", alignItems: "baseline", marginTop: marginFromTop}}>
+            {title}
+            {value}
+        </div>
+    );
+}
+
+export const ReportRowSimple: React.FC<{
+    title: string,
+    titleSize?: string,
+    value: string,
+    secondaryValue?: string,
+    titleColor?: string,
+    marginFromTop?: string,
+}> = ({title, titleSize, value, secondaryValue, titleColor, marginFromTop}) => {
+    return (
+        <ReportRowCustom title={<h4 style={{color: titleColor, fontSize: titleSize}}>{title}</h4>} value={
+            <h6>
+                {value} <span style={{color: '#4c5c60'}}>{secondaryValue}</span>
+            </h6>
+        }
+        marginFromTop={marginFromTop}
+        />
+    );
+}
+
+export const ReportRowSimpleWithColor: React.FC<{
+    title: string,
+    value: string,
+    valueColor: string,
+}> = ({title, value, valueColor}) => {
+    return (
+        <ReportRowCustom title={<h4>{title}</h4>} value={
+            <h6 style={{color: valueColor}}>
+                {value}
+            </h6>
+        }/>
+    );
+}
+
+export const InfoFeature: React.FC<{
+    logo: string,
+    logoColor: string,
+    percent: string,
+    title: string,
+    value: string,
+}> = ({logo, logoColor, percent, title, value}) => {
+    return (
+
+        <div style={{display: 'flex'}}>
+            <div style={{width: '70px', height: '70px', borderRadius: '25px', backgroundColor: logoColor}}>
+                <div
+                    className="donut-container"
+                    style={{textAlign: 'center'}}
+                >
+                    <i className={logo} style={{color: '#ffffff', fontSize: '200%'}}/>
+                </div>
+            </div>
+            <div style={{display: 'inline', marginLeft: '15px'}}>
+                <div style={{color: parseInt(percent) < 0 ? '#a078b3' : '#7daab5'}}>
+                    {parseInt(percent) < 0 ? '- ' + percent.substring(1) : '+ ' + percent}
+                </div>
+                <h4>
+                    {title}
+                </h4>
+                <div style={{fontWeight: 'bold', marginTop: '-8px'}}>
+                    {value}
+                </div>
+            </div>
+        </div>
+    );
+}
--- frontend/src/pages/Account.tsx
+++ frontend/src/pages/Account.tsx
@@ -1,5 +1,8 @@
 import React, {} from 'react';
 import {SectionTitle} from "../components/SectionTitle";
+import {GridComponent} from "../components/Grid";
+import {MainpageCard} from "../components/Card";
+import {ReportRowCustom, ReportRowSimple} from "../components/SectionBody";
 const title_img =  require("../assets/account_image.png");
 
 export function Account () {
@@ -9,14 +12,79 @@ export function Account () {
                 style={{gridColumn: "1 / span 8", width: "100%"}}
                 img={title_img}
                 title={"Account"}
-                subtitle={<h6>This is a subtitle...</h6>}
+                subtitle={<h6>Last updated &nbsp;&nbsp; 09.05.2023</h6>}
             />
         );
     }
 
+    function Profile() {
+        return (
+            <MainpageCard style={{gridColumn: "1 / span 8", gridRow: "2"}}>
+                <ReportRowCustom
+                    title={<h3>{'Profile'}</h3>}
+                    value={
+                        <div style={{
+                            fontSize: "1.1em",
+                            fontWeight: "bold",
+                            color: "#7596a1",
+                            cursor: "pointer"
+                        }}>
+                            <i className="bi bi-pencil-square"/>
+                            &nbsp;&nbsp;Edit information
+                        </div>
+                    }
+                />
+                <ReportRowSimple title={'Navn'} value={"Soren Toennesen"}/>
+                <ReportRowSimple title={'Telefonnummer'} value={"+45 12 34 56 78"}/>
+                <ReportRowSimple title={'Mail'} value={"sorentoennesen@gmail.com"}/>
+            </MainpageCard>
+        );
+    }
+
+    function Password() {
+        return (
+            <MainpageCard style={{gridColumn: "1 / span 8", gridRow: "3"}}>
+                <ReportRowCustom title={<h3>{'Password'}</h3>} value={
+                    <div style={{
+                        fontSize: "1.1em",
+                        fontWeight: "bold",
+                        color: "#7596a1",
+                        cursor: "pointer"
+                    }}>
+                        <i className="bi bi-pencil-square"/>
+                        &nbsp;&nbsp;Edit information
+                    </div>
+                }/>
+
+                <ReportRowSimple title={'Current password'} value={'****************'}/>
+            </MainpageCard>
+        );
+    }
+
+    function ProfileType() {
+        return (
+            <MainpageCard style={{gridColumn: "1 / span 8", gridRow: "4", marginBottom: "30px"}}>
+                <ReportRowCustom title={<h3>{'Profile type'}</h3>} value={<></>}/>
+                <ReportRowSimple title={'Profile'} value={'Administrator'}/>
+                <ReportRowSimple title={'Active since'} value={'01-03-2023'}/>
+                <ReportRowSimple title={'Abbonement'} value={'Active'}/>
+            </MainpageCard>
+        );
+    }
+
     return (
-        <>
-            <Header />
-        </>
+        <div style={{
+            display: "flex",
+            flex: '1',
+            alignItems: 'stretch',
+            flexFlow: 'column'
+        }}>
+            <GridComponent>
+                <Header/>
+                <Profile/>
+                <Password/>
+                <ProfileType/>
+            </GridComponent>
+        </div>
     )
 }
